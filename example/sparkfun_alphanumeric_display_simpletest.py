47359a1cf86564dd78ee903686a0e88f0416d294
Neradoc <neraOnGit@ri1.fr>
2023-04-25 05:43:39 +0200

    all characters fixed ?

--- example/sparkfun_alphanumeric_display_simpletest.py
+++ example/sparkfun_alphanumeric_display_simpletest.py
@@ -1,24 +1,57 @@
 import board
 import time
-from sparkfun_alphanumeric_display import SparkFunQwiicDisplay
+from sparkfun_alphanumeric_display import *
+
+from digitalio import *
+button = DigitalInOut(board.BUTTON_SELECT)
+button.pull = Pull.DOWN
 
 i2c = board.I2C()
 display = SparkFunQwiicDisplay(i2c)
 
 while True:
-    for char in "11111 77777 ":
-        display.show_letter(char)
-        time.sleep(0.3)
+    display.print(">")
 
-for num in range(32):
-    char = chr(num + 32)
-    display.show_letter(char)
-    time.sleep(0.1)
+display.print("Hi !")
+time.sleep(0.5)
+display.blink_rate = 0
+display.brightness = 1
 
-# value = 0
-# for i in range(8*16):
-#     value = value | (1 << i)
-#     data = value.to_bytes(16, "little")
-#     print(i, bin(i), data)
-#     bus.write(b"\x00" + data)
-#     time.sleep(0.01)
+while True:
+#     for char in "17AfeiI":
+#         display.write(char * 4)
+#         time.sleep(0.3)
+#         display.write("")
+#         time.sleep(0.05)
+
+    for num in range(len(SEGMENTS)):
+        char = chr(num + 32)
+        print(char, end="")
+        display.print(char * 4)
+        while button.value is True:
+            time.sleep(0.01)
+        while button.value is False:
+            time.sleep(0.01)
+        time.sleep(0.01)
+
+    print("\n- dot", "-"*50)
+    display.dot = True
+
+    for num in range(len(SEGMENTS)):
+        char = chr(num + 32)
+        print(char, end="")
+        display.print(char * 4)
+        time.sleep(0.01)
+
+    print("\n- colon", "-"*48)
+    display.colon = True
+
+    for num in range(len(SEGMENTS)):
+        char = chr(num + 32)
+        print(char, end="")
+        display.print(char * 4)
+        time.sleep(0.01)
+
+    print("\n- none", "-"*49)
+    display.dot = False
+    display.colon = False
--- sparkfun_alphanumeric_display.py
+++ sparkfun_alphanumeric_display.py
@@ -4,98 +4,98 @@ SEGMENTS = [
     0b00000000000000, # ' ' (space)
     0b00001000001000, # '!'
     0b00001000000010, # '"'
-    0b01001101001110, # '#'
-    0b01001101101101, # '$'
+    0b01001011001110, # '#'
+    0b01001011101101, # '$'
     0b10010000100100, # '%'
-    0b00110011011001, # '&'
+    0b00110101011001, # '&'
     0b00001000000000, # '''
     0b00000000111001, # '('
     0b00000000001111, # ')'
-    0b11111010000000, # '*'
-    0b01001101000000, # '+'
+    0b11111100000000, # '*'
+    0b01001011000000, # '+'
     0b10000000000000, # ','
-    0b00000101000000, # '-'
+    0b00000011000000, # '-'
     0b00000000000000, # '.'
     0b10010000000000, # '/'
     0b00000000111111, # '0'
     0b00010000000110, # '1'
-    0b00000101011011, # '2'
-    0b00000101001111, # '3'
-    0b00000101100110, # '4'
-    0b00000101101101, # '5'
-    0b00000101111101, # '6'
+    0b00000011011011, # '2'
+    0b00000011001111, # '3'
+    0b00000011100110, # '4'
+    0b00000011101101, # '5'
+    0b00000011111101, # '6'
     0b01010000000001, # '7'
-    0b00000101111111, # '8'
-    0b00000101100111, # '9'
+    0b00000011111111, # '8'
+    0b00000011100111, # '9'
     0b00000000000000, # ':'
     0b10001000000000, # ';'
     0b00110000000000, # '<'
-    0b00000101001000, # '='
-    0b01000010000000, # '>'
-    0b01000100000011, # '?'
-    0b00001100111011, # '@'
-    0b00000101110111, # 'A'
-    0b01001100001111, # 'B'
+    0b00000011001000, # '='
+    0b10000100000000, # '>'
+    0b01000010000011, # '?'
+    0b00001010111011, # '@'
+    0b00000011110111, # 'A'
+    0b01001010001111, # 'B'
     0b00000000111001, # 'C'
     0b01001000001111, # 'D'
-    0b00000101111001, # 'E'
-    0b00000101110001, # 'F'
-    0b00000100111101, # 'G'
-    0b00000101110110, # 'H'
+    0b00000011111001, # 'E'
+    0b00000011110001, # 'F'
+    0b00000010111101, # 'G'
+    0b00000011110110, # 'H'
     0b01001000001001, # 'I'
     0b00000000011110, # 'J'
     0b00110001110000, # 'K'
     0b00000000111000, # 'L'
-    0b00010010110110, # 'M'
-    0b00100010110110, # 'N'
+    0b00010100110110, # 'M'
+    0b00100100110110, # 'N'
     0b00000000111111, # 'O'
-    0b00000101110011, # 'P'
+    0b00000011110011, # 'P'
     0b00100000111111, # 'Q'
-    0b00100101110011, # 'R'
+    0b00100011110011, # 'R'
     0b00000110001101, # 'S'
     0b01001000000001, # 'T'
     0b00000000111110, # 'U'
     0b10010000110000, # 'V'
     0b10100000110110, # 'W'
-    0b10110010000000, # 'X'
-    0b01010010000000, # 'Y'
+    0b10110100000000, # 'X'
+    0b01010100000000, # 'Y'
     0b10010000001001, # 'Z'
     0b00000000111001, # '['
-    0b00100010000000, # '\'
+    0b00100100000000, # '\'
     0b00000000001111, # ']'
     0b10100000000000, # '^'
     0b00000000001000, # '_'
-    0b00000010000000, # '`'
-    0b00000101011111, # 'a'
+    0b00000100000000, # '`'
+    0b00000011011111, # 'a'
     0b00100001111000, # 'b'
-    0b00000101011000, # 'c'
-    0b10000100001110, # 'd'
+    0b00000011011000, # 'c'
+    0b10000010001110, # 'd'
     0b00000001111001, # 'e'
     0b00000001110001, # 'f'
     0b00000110001111, # 'g'
-    0b00000101110100, # 'h'
+    0b00000011110100, # 'h'
     0b01000000000000, # 'i'
     0b00000000001110, # 'j'
     0b01111000000000, # 'k'
     0b01001000000000, # 'l'
-    0b01000101010100, # 'm'
+    0b01000011010100, # 'm'
     0b00100001010000, # 'n'
-    0b00000101011100, # 'o'
+    0b00000011011100, # 'o'
     0b00010001110001, # 'p'
-    0b00100101100011, # 'q'
+    0b00100011100011, # 'q'
     0b00000001010000, # 'r'
     0b00000110001101, # 's'
     0b00000001111000, # 't'
     0b00000000011100, # 'u'
     0b10000000010000, # 'v'
     0b10100000010100, # 'w'
-    0b10110010000000, # 'x'
-    0b00001100001110, # 'y'
+    0b10110100000000, # 'x'
+    0b00001010001110, # 'y'
     0b10010000001001, # 'z'
-    0b10000011001001, # '{'
+    0b10000101001001, # '{'
     0b01001000000000, # '|'
-    0b00110100001001, # '}'
-    0b00000101010010, # '~'
+    0b00110010001001, # '}'
+    0b00000011010010, # '~'
     0b11111111111111, # Unknown character (DEL or RUBOUT)
 ]
 
@@ -133,29 +133,65 @@ buffer0 = bytearray(
 )
 
 class SparkFunQwiicDisplay:
-    def __init__(self, i2c, address=0x70):
+    def __init__(self, i2c, address=0x70, brightness=0xF):
         self.device = i2c_device.I2CDevice(i2c, address)
         self._duty = 15
         self._blink_rate = ALPHA_BLINK_RATE_NOBLINK
+        self._duty = brightness & 0xF
+        self._display_on = True
+        self._colon = False
+        self._dot = False
+        self._text = ""
         self.setup()
 
     @property
     def brightness(self):
+        """
+        Brightness is a value between 0 and 15.
+        Set to None in order to turn off the display.
+        """
         return self._duty
 
     @brightness.setter
     def brightness(self, brightness):
-        self._duty = brightness & 0xF
+        if brightness is None:
+            self._display_on = False
+            self.setup()
+        else:
+            self._display_on = True
+            self._duty = brightness & 0xF
+            self.setup()
 
     @property
     def blink_rate(self):
-        raise NotImplementedError()
         return self._blink_rate
 
     @blink_rate.setter
     def blink_rate(self, blink_rate):
-        raise NotImplementedError()
-        self._blink_rate = ALPHA_BLINK_RATE_NOBLINK
+        blink_rate = blink_rate & 0b11
+        if blink_rate:
+            self._blink_rate = 0b100 - blink_rate
+        else:
+            self._blink_rate = ALPHA_BLINK_RATE_NOBLINK
+        self.setup()
+
+    @property
+    def colon(self):
+        return self._colon
+
+    @colon.setter
+    def colon(self, on):
+        self._colon = bool(on)
+        self.refresh()
+
+    @property
+    def dot(self):
+        return self._dot
+
+    @dot.setter
+    def dot(self, on):
+        self._dot = bool(on)
+        self.refresh()
 
     def setup(self):
         with self.device as bus:
@@ -164,16 +200,26 @@ class SparkFunQwiicDisplay:
             command = (
                 ALPHA_CMD_DISPLAY_SETUP
                 | (self._blink_rate << 1)
-                | ALPHA_DISPLAY_ON
+                | int(self._display_on)
             )
             bus.write(bytes([command]))
 
-    def show_letter(self, char):
+    def refresh(self):
+        self.print(self._text)
+
+    def print(self, text):
+        self._text = text
         buffer = bytearray(16)
-        segment = SEGMENTS[ord(char) - ord(" ")]
-        for i in range(8):
-            pos = i * 2
-            buffer[pos] = (segment >> i) & 1 | ((segment >> (i + 8)) & 1) * 0x10
-        print(buffer)
+        for pos, char in enumerate(text[:4]):
+            segment = SEGMENTS[ord(char) - ord(" ")]
+            for i in range(7):
+                digit = (segment >> i) & 1
+                digit |= ((segment >> (i + 7)) & 1) << 4
+                buffer[i * 2] |= digit * (1 << pos)
+            if pos == 0:
+                if self._colon:
+                    buffer[1] |= 1
+                if self._dot:
+                    buffer[3] |= 1
         with self.device as bus:
             bus.write(b"\x00" + bytes(buffer))
